f39e1251d305004b9a3e476de2ccffdecc7ef8d3
mbz4 <30321314+mbz4@users.noreply.github.com>
2026-05-01 02:04:14 +0300

    patch, simplify

--- include/AnimationPlayer.h
+++ include/AnimationPlayer.h
@@ -3,10 +3,6 @@
 
 #include <Arduino.h>
 
-void switchFolder(const String& newFolder);
-void loadMjpegFilesList();
-void playSelectedMjpeg(int mjpegIndex);
-void updateAnimationPlayer();
-void used_to_be_setup();
+void initAnimationPlayer();
 
 #endif
